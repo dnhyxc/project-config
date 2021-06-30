63cc3da4a6015dad72d61f3fa53cc9f1bf2b74ab
dnhyxc <2640559435@qq.com>
2021-06-30 20:18:59 +0800

    add antd

--- src/routes/home/index.tsx
+++ src/routes/home/index.tsx
@@ -1,9 +1,12 @@
 import React from 'react';
+import { Button } from 'antd';
 import styles from './index.less';
 
 const Home: React.FC = () => {
   return (
-    <div className={styles.container}>Home</div>
+    <div className={styles.container}>
+      <Button type="primary">Home</Button>
+    </div>
   );
 };
 
